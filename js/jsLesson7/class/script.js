b47cb6b02317ac0f8d702cc83db2cc9cc8ea8efc
muskiv <muskiv21@gmail.com>
2021-11-27 20:44:03 +0200

    class 100%

--- js/jsLesson7/class/script.js
+++ js/jsLesson7/class/script.js
@@ -52,7 +52,46 @@ myCar.addDriver('Володимир');
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
 
+/*
+class Automobile{
+    constructor (car, model, year, maxSpeed,    engineCapacity) {
+        this.car = car;
+        this.model = model;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.engineCapacity = engineCapacity;
+    
 
+//============================================================================================================
+    this.drive = function () {
+    console.log(`Їдемо зі швидкістю ${this.maxSpeed} на годину`);
+    };
+    this.info = function () {
+        console.log(`Car - ${this.car}, Model - ${this.model}, Year - ${this.year}, MaxSpeed - ${this.maxSpeed}, EngineCapacity - ${this.engineCapacity},`);
+    };
+    this.increaseMaxSpeed = function (newSpeed) {
+        console.log(`Прокачали двигун до максимума ${this.maxSpeed + newSpeed}`);
+    };
+    this.changeYear = function (newValue) {
+        this.year = newValue;
+        console.log(`Взяли новеньку з салону ${this.year}`);
+    };
+    this.addDriver = function (driver) {
+        this.addDriver = driver;
+        console.log(`Водій ${this.addDriver}`);
+    };
+    };
+};
+
+let myCar = new Automobile('Ford', 'Focus', 2017, 192, '2.0 L');
+
+console.log(myCar);
+myCar.drive();
+myCar.info();
+myCar.increaseMaxSpeed(48);
+myCar.changeYear(2021);
+myCar.addDriver('Володимир');
+*/
 
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
