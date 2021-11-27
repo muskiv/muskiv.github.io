c1f89375f3457553af219b199eafbd1b5b4764ae
muskiv <muskiv21@gmail.com>
2021-11-27 18:27:27 +0200

    class 66.66%

--- js/jsLesson7/class/script.js
+++ js/jsLesson7/class/script.js
@@ -7,7 +7,7 @@
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
 
 /*
-function automobile(car, model, year, maxSpeed, engineCapacity) {
+function Automobile(car, model, year, maxSpeed, engineCapacity) {
     this.car = car;
     this.model = model;
     this.year = year;
@@ -33,7 +33,7 @@ function automobile(car, model, year, maxSpeed, engineCapacity) {
     };
 };
 
-let myCar = new automobile('Ford', 'Focus', 2017, 192, '2.0 L');
+let myCar = new Automobile('Ford', 'Focus', 2017, 192, '2.0 L');
 
 
 console.log(myCar);
@@ -58,3 +58,55 @@ myCar.addDriver('Володимир');
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
 // За допомоги циклу знайти яка попелюшка повинна бути з принцом.
 // Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+
+/*
+function Cinderella(name, age, sizeFeet) {
+    this.name = name;
+    this.age = age;
+    this.sizeFeet = sizeFeet;
+};
+
+
+let woman = [
+    new Cinderella('Olga', 35, 45),
+    new Cinderella('Vika', 32, 43),
+    new Cinderella('Kate', 30, 40),
+    new Cinderella('Ira', 37, 45),
+    new Cinderella('Luba', 35, 45),
+    new Cinderella('Khris', 30, 41),
+    new Cinderella('Olga', 31, 42),
+    new Cinderella('Uliana', 25, 39),
+    new Cinderella('Lena', 28, 43),
+    new Cinderella('Olenka', 26, 46),
+];
+
+console.log(woman);
+
+//==============================================================
+
+function Prince(name, age, foundShoe) {
+    this.name = name;
+    this.age = age;
+    this.foundShoe = foundShoe;
+};
+
+let richMan = new Prince('Volodymyr', 26, 39);
+console.log(richMan);
+
+//==============================================================
+
+let findCinderella = (woman ,richMan) =>{
+    for (const iterator of woman) {
+        if (iterator.sizeFeet === richMan.foundShoe) {
+            return `Імя попелюшки, якої нога залізла в кірзак ${iterator.name}`
+        };
+    };
+};
+
+console.log(findCinderella(woman ,richMan));
+
+//==============================================================
+
+let trueCinderella = woman.find((x) => x.sizeFeet === richMan.foundShoe);
+console.log(trueCinderella);
+*/
