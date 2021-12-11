ff3e7fa50e69c27fa508e173082cbf1460007386
muskiv <muskiv21@gmail.com>
2021-12-11 17:25:03 +0200

    home + class done

--- js/jsLesson11/home/script.js
+++ js/jsLesson11/home/script.js
@@ -40,3 +40,43 @@
 //     array.push(inputModel.value, inputType.value, inputVolume.value);
 //     localStorage.setItem('array', JSON.stringify(array));
 // };
+
+
+// let model = document.forms.carForm.model; 
+//     let type = document.forms.carForm.type; 
+//     let volume = document.forms.carForm.volume; 
+//     let btnCar = document.getElementById('btnCar'); 
+//     let item = 'keyCar'; 
+ 
+//     let saveCar = (modelCar, typeCar, volumeCar) => { 
+//         let newArr = JSON.parse(localStorage.getItem(item)) || []; 
+ 
+//         newArr.push({modelCar, typeCar, volumeCar}) 
+//         localStorage.setItem(item, JSON.stringify(newArr)); 
+//     }; 
+ 
+//     btnCar.onclick = () => { 
+//         saveCar(model.value, type.value, volume.value); 
+//     }
+
+//     let form = document.createElement('form'); 
+
+// let inputModel = document.createElement('input');
+// let inputType = document.createElement('input');
+// let inputVolume = document.createElement('input');
+// let button = document.createElement('button');
+// button.innerText = ('Відправити');
+// let item = 'keyCar';
+
+// form.append(inputModel, inputType,  inputVolume, button);
+// document.body.appendChild(form);
+
+// let saveCar = (inputModel, inputType, inputVolume) => {
+//     let newArr = JSON.parse(localStorage.getItem(item)) || [];
+//     newArr.push({inputModel, inputType, inputVolume});
+//     localStorage.setItem(item, JSON.stringify(newArr));
+// };
+
+// button.onclick = function () {
+//     saveCar(inputModel.value, inputType.value, inputVolume.value);
+// };
