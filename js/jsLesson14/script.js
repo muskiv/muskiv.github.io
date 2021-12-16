19dc56299092abb0c74668b3507d4563ad0550cd
muskiv <muskiv21@gmail.com>
2021-12-16 19:30:13 +0200

    done

--- js/jsLesson14/script.js
+++ js/jsLesson14/script.js
@@ -1,4 +1,3 @@
-
 //  TODO HW
 // Зробити свій розпорядок дня. //
 // У вас має бути більше 10 асинхронних дій з рандомними затримками.
@@ -7,24 +6,23 @@
 // // Напиклад. // Прикнутись - 0.3с // Поснідати - 1с // Піти в душ - 0.5с // Дочекатись автобус - 3с // Пообідати - 1с // // І так далі
 
 
-function wakeUp (beUp){
+function wakeUp(beUp) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(beUp){
+        setTimeout(() => {
+            if (beUp) {
                 console.log('Нарешті я прокинувся');
                 resolve('Наступна дія');
-            } 
-            else {
+            } else {
                 reject('вставай, час вчитись');
             }
         }, 300);
     });
 };
 
-function toilet (urination){
+function toilet(urination) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(urination){
+        setTimeout(() => {
+            if (urination) {
                 console.log('Ого! Ти сьогодні снайпер)');
                 resolve('Наступна дія');
             } else {
@@ -34,24 +32,23 @@ function toilet (urination){
     });
 };
 
-function teeth (brushedHisTeeth){
+function teeth(brushedHisTeeth) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
+        setTimeout(() => {
             if (brushedHisTeeth) {
                 console.log('Супер, я почистив зуби');
                 resolve('Наступна дія');
-            }
-            else {
+            } else {
                 reject('Жуйка не допоможе');
             }
         }, 100);
     });
 };
 
-function clothing (putOnClothes){
+function clothing(putOnClothes) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(putOnClothes){
+        setTimeout(() => {
+            if (putOnClothes) {
                 console.log('Молодець, що не в трусах кодиш)');
                 resolve('Наступна дія');
             } else {
@@ -61,10 +58,10 @@ function clothing (putOnClothes){
     });
 };
 
-function water (drink){
+function water(drink) {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            if(drink){
+        setTimeout(() => {
+            if (drink) {
                 console.log('Воду випив, баланс відновлено');
                 resolve('Наступна дія');
             } else {
@@ -74,24 +71,23 @@ function water (drink){
     });
 };
 
-function eat (hungry){
-    return new Promise((resolve, reject) =>{
-        setTimeout(() =>{
-            if(hungry){
+function eat(hungry) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (hungry) {
                 console.log('Яєшня в тобі, значить ти ситий)');
                 resolve('Наступна дія');
-            }
-            else {
+            } else {
                 reject('Голодний');
             }
         }, 300);
     });
 };
 
-function wentToStudy (study){
-    return new Promise((resolve,reject) =>{
-        setTimeout(()=>{
-            if(study){
+function wentToStudy(study) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (study) {
                 console.log('Буде з тебе програміст)');
                 resolve('Наступна дія');
             } else {
@@ -101,70 +97,102 @@ function wentToStudy (study){
     });
 };
 
-function turnOnTheComputer (сomputer){
+function turnOnTheComputer(сomputer) {
     return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-        if(сomputer){
-            console.log('Включити ноут, підготувати всі проги)');
-            resolve('Наступна дія');
-        } else {
-            reject('Ой, а де зярядка для ноута');
-        }
+        setTimeout(() => {
+            if (сomputer) {
+                console.log('Включити ноут, підготувати всі проги)');
+                resolve('Наступна дія');
+            } else {
+                reject('Ой, а де зярядка для ноута');
+            }
         }, 100);
     });
 };
 
-function cats (spendTimeWithCats){
+function cats(spendTimeWithCats) {
     return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-        if(spendTimeWithCats){
-            console.log('Коти люблять ласку, бавитись, їсти, все решта без тебе)');
-            resolve('Наступна дія');
-        } else {
-            reject('Спить?');
-        }
+        setTimeout(() => {
+            if (spendTimeWithCats) {
+                console.log('Коти люблять ласку, бавитись, їсти, все решта без тебе)');
+                resolve('Наступна дія');
+            } else {
+                reject('Спить?');
+            }
         }, 300);
     });
 };
 
-function wife (time){
+function wife(time) {
     return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-        if(time){
-            console.log('Жінка, майже як кіт, тільки більше участі в житті)');
-            resolve('Наступна дія');
-        } else {
-            reject('Ой не то сказав їй');
-        }
+        setTimeout(() => {
+            if (time) {
+                console.log('Жінка, майже як кіт, тільки більше участі в житті)');
+                resolve('Наступна дія');
+            } else {
+                reject('Ой не то сказав їй');
+            }
         }, 200);
     });
 };
 
-function sleep (bed){
+function sleep(bed) {
     return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-        if(bed){
-            console.log('Молодець, гарний був день');
-            resolve('Наступна дія');
-        } else {
-            reject('Глянути серіал');
-        }
+        setTimeout(() => {
+            if (bed) {
+                console.log('Молодець, гарний був день');
+                resolve('Наступна дія');
+            } else {
+                reject('Глянути серіал');
+            }
         }, 500);
     });
 };
 
-async function day(){
-    let q = await wakeUp(true);
-    let w = await toilet(true);
-    let e = await teeth(true);
-    let r = await water(true);
-    let t = await eat(true);
-    let y = await clothing(true);
-    let u = await turnOnTheComputer(true);
-    let i = await wentToStudy(true);
-    let o = await cats(true);
-    let p = await wife(true);
-    let a = await sleep(true);
-}
+function work(workDone, callback) {
+    return new Promise((resolve) => {
+        let dolar = 0;
+        if (workDone) {
+            console.log('О ланадосікі');
+            dolar += 5000;
+            callback(dolar);
+            resolve();
+        } else {
+            console.log('Кинули на бабло');
+            callback(dolar);
+            resolve();
+        };
+    });
+};
+
+function wentSkiing(cash) {
+    return new Promise((resolve, reject) => {
+        if (cash > 0) {
+            console.log('Гори кличуть))))');
+            resolve();
+        } else {
+            reject('Братан, не можу поїхати, бабла нема(');
+        };
+    });
+};
+
+async function day() {
+    try {
+        let q = await wakeUp(true);
+        let w = await toilet(true);
+        let e = await teeth(true);
+        let r = await water(true);
+        let t = await eat(true);
+        let y = await clothing(true);
+        let u = await turnOnTheComputer(true);
+        let i = await wentToStudy(true);
+        let o = await cats(true);
+        let p = await wife(true);
+        let a = await sleep(true);
+        let s = await work(true, wentSkiing);
+    } catch (e) {
+        console.error(e);
+    };
+};
 
 day();
