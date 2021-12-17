b8d5ad5e7ab3f7894254c178f5fb281f239834fa
muskiv <muskiv21@gmail.com>
2021-12-17 22:40:35 +0200

    done

--- js/jsLesson14/script.js
+++ js/jsLesson14/script.js
@@ -224,43 +224,172 @@
 //     });
 
 
-// через call backs
-setTimeout(()=> {
-    console.log('Нарешті я прокинувся');
+// через call backs============================================================================================================================================================================================================================
+
+function wakeUp(beUp, cb) {
     setTimeout(() => {
-        console.log('Ого! Ти сьогодні снайпер');
-        setTimeout(() => {
-            console.log('Супер, я почистив зуби');
-            setTimeout(() => {
-                console.log('Воду випив, баланс відновлено');
-                setTimeout(() => {
-                    console.log('Яєшня в тобі, значить ти ситий');
-                    setTimeout(() => {
-                        console.log('Молодець, що не в трусах кодиш');
-                        setTimeout(() => {
-                            console.log('Включити ноут, підготувати всі проги');
-                            setTimeout(() => {
-                                console.log('Буде з тебе програміст)');
-                                setTimeout(() => {
-                                    console.log('Коти люблять ласку, бавитись, їсти, все решта без тебе)');
-                                    setTimeout(() => {
-                                        console.log('Жінка, майже як кіт, тільки більше участі в житті)');
-                                        setTimeout(() => {
-                                            console.log('Молодець, гарний був день');
-                                            setTimeout(() => {
-                                                console.log('О ланадосікі');
-                                                setTimeout(() => {
-                                                    console.log('Гори кличуть))))');
-                                                }, 100);
-                                            }, 200);
-                                        }, 400);
-                                    }, 200);
-                                }, 600);
-                            }, 300);
-                        }, 200);
-                    }, 600);
-                }, 300);
-            }, 400);
-        }, 400);
+        if (beUp) {
+            cb(null, 'Нарешті я прокинувся');
+        } else {
+            cb('вставай, час вчитись');
+        }
+    }, 300);
+};
+
+function toilet(urination, cb) {
+    setTimeout(() => {
+        if (urination) {
+            cb(null, 'Ого! Ти сьогодні снайпер)');
+        } else {
+            cb('Мимо, тепер санітарне прибирання(((');
+        }
+    }, 400);
+};
+
+function teeth(brushedHisTeeth, cb) {
+    setTimeout(() => {
+        if (brushedHisTeeth) {
+            cb(null, 'Супер, я почистив зуби');
+        } else {
+            cb('Жуйка не допоможе');
+        }
     }, 100);
-}, 500);
+};
+
+function clothing(putOnClothes, cb) {
+    setTimeout(() => {
+        if (putOnClothes) {
+            cb(null, 'Молодець, що не в трусах кодиш)');
+        } else {
+            cb('Дурачок чи шо? Голий підеш?');
+        }
+    }, 500);
+};
+
+function water(drink, cb) {
+    setTimeout(() => {
+        if (drink) {
+            cb(null, 'Воду випив, баланс відновлено');
+        } else {
+            cb('Сушняк буде...');
+        }
+    }, 250);
+};
+
+function eat(hungry, cb) {
+    setTimeout(() => {
+        if (hungry) {
+            cb(null, 'Яєшня в тобі, значить ти ситий)');
+        } else {
+            cb('Голодний');
+        }
+    }, 300);
+};
+
+function wentToStudy(study, cb) {
+    setTimeout(() => {
+        if (study) {
+            cb(null, 'Буде з тебе програміст)');
+        } else {
+            cb('Тобі руки поламати? БІГОМ ВЧИТИСЬ!');
+        }
+    }, 1000);
+};
+
+function turnOnTheComputer(сomputer, cb) {
+    setTimeout(() => {
+        if (сomputer) {
+            cb(null, 'Включити ноут, підготувати всі проги)');
+        } else {
+            cb('Ой, а де зярядка для ноута');
+        }
+    }, 100);
+};
+
+function cats(spendTimeWithCats, cb) {
+    setTimeout(() => {
+        if (spendTimeWithCats) {
+            cb(null, 'Коти люблять ласку, бавитись, їсти, все решта без тебе)');
+        } else {
+            cb('Спить?');
+        }
+    }, 300);
+};
+
+function wife(time, cb) {
+    setTimeout(() => {
+        if (time) {
+            cb(null, 'Жінка, майже як кіт, тільки більше участі в житті)');
+        } else {
+            cb('Ой не то сказав їй');
+        }
+    }, 200);
+};
+
+function sleep(bed, cb) {
+    setTimeout(() => {
+        if (bed) {
+            cb(null, 'Молодець, гарний був день');
+        } else {
+            cb('Глянути серіал');
+        }
+    }, 500);
+};
+
+
+wakeUp(true, (error, ok) => {
+    if (!error) {
+        console.log(ok);
+        toilet(true, (error, ok) => {
+            if (!error) {
+                console.log(ok);
+                teeth(true, (error, ok) => {
+                    if (!error) {
+                        console.log(ok);
+                        water(true, (error, ok) => {
+                            if (!error) {
+                                console.log(ok);
+                                eat(true, (error, ok) => {
+                                    if (!error) {
+                                        console.log(ok);
+                                        clothing(true, (error, ok) => {
+                                            if (!error) {
+                                                console.log(ok);
+                                                wentToStudy(true, (error, ok) => {
+                                                    if (!error) {
+                                                        console.log(ok);
+                                                        turnOnTheComputer(true, (error, ok) => {
+                                                            if (!error) {
+                                                                console.log(ok);
+                                                                cats(true, (error, ok) => {
+                                                                    if (!error) {
+                                                                        console.log(ok);
+                                                                        wife(true, (error, ok) => {
+                                                                            if (!error) {
+                                                                                console.log(ok);
+                                                                                sleep(true, (error, ok) => {
+                                                                                    if (!error) {
+                                                                                        console.log(ok);
+                                                                                        sleep
+                                                                                    };
+                                                                                });
+                                                                            };
+                                                                        });
+                                                                    };
+                                                                });
+                                                            };
+                                                        });
+                                                    };
+                                                });
+                                            };
+                                        });
+                                    };
+                                });
+                            };
+                        });
+                    };
+                });
+            };
+        });
+    };
+});
