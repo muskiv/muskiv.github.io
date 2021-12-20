3dfbef0f33ee0608eb3617b0485100ed3db2a1b6
muskiv <muskiv21@gmail.com>
2021-12-20 06:02:14 +0200

    mini project done

--- js/mini-project/script.js
+++ js/mini-project/script.js
@@ -1,9 +1,8 @@
-// В index.html
-// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
-// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
-// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
-// На странице user-details.html:
-// 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
+let containet = document.createElement('div');
+containet.style.display = 'flex';
+containet.style.flexWrap = 'wrap';
+containet.style.justifyContent = 'space-evenly';
+
 
 
 fetch('https://jsonplaceholder.typicode.com/users')
@@ -13,34 +12,59 @@ fetch('https://jsonplaceholder.typicode.com/users')
         for (const iterator of json) {
             let a = document.createElement('a');
             a.href = 'user-details.html';
-            a.innerText = `user-details`;
+            a.innerText = 'user-details';
             a.style.border = 'solid';
-            a.style.marginLeft = '2%';
             a.style.paddingLeft = '5px';
-            a.style.paddingRight = '5px';
+            a.style.marginTop = '10px';
             a.style.textDecoration = 'none';
             a.style.color = 'black';
+            a.style.width = '80px';
+            a.style.background = 'white';
 
             let id = document.createElement('div');
-            id.innerText = `id ${iterator.id} name ${iterator.name}`;
-            id.style.marginTop = '2%';
-            
+            id.innerText = `${iterator.id} ${iterator.name}`;
+            id.style.width = '34%';
+            id.style.height = '100px';
+            id.style.display = 'flex';
+            id.style.flexWrap = 'wrap';
+            id.style.flexDirection = 'column';
+            id.style.alignContent = 'center';
+            id.style.alignItems = 'center';
+            id.style.justifyContent = 'center';
+            id.style.marginBottom = '10px';
+            id.style.marginLeft = '5px';
+            id.style.marginRight = '5px';
+            id.style.background = '#c47ff5';
+
             a.onclick = function () {
                 user.push(...Object.values(iterator));
                 localStorage.setItem('user', JSON.stringify(user));
             }
 
             id.append(a);
-            document.body.append(id);
-        }
+            containet.append(id);
+            document.body.append(containet);
+        };
     });
 
-// 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
+// В index.html
+// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
+
+// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
+
+// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
+
+// На странице user-details.html:
+// 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
+
+// 5 авДобить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
 // (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+
 // 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
 
 // На странице post-details.html:
 // 7 Вывести всю, без исключения, информацию про объект post на кнопку/ссылку которого был совершен клик ранее.
+
 // 8 Ниже информации про пост, вывести все комментарии текущего поста (эндпоинт для получения информации - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
 
 // Стилизация проекта -
