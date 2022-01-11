50d5e0162129a83452947cbb0f753b3298621e74
muskiv <muskiv21@gmail.com>
2022-01-12 01:39:46 +0200

    done

--- react-app/src/components/api.js
+++ react-app/src/components/api.js
@@ -1,20 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import Spacex from "./spaceX";
 
-import{useEffect, useState} from 'react';
-import spaceX from './spaceX'
-
-const SpaceX = () => {
+const Space = () => {
     let [spaceX, setSpaceX] = useState([]);
 
     useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches/')
         .then(value => value.json())
-        .then(value => setSpaceX(value))
+        .then(spaceX => {
+            let not_tztz = spaceX.filter(spaceX => spaceX.launch_year !== '2020');
+            setSpaceX(not_tztz);
+            })
+
+        
+
     }, [])
     return (
         <div className='api'>
-            {spaceX.map(value => <SpaceX mission_name={value.mission_name} launch_year={value.launch_year} mission_patch_small={value.mission_patch_small}/>)}
+            {spaceX.map((value, index) => <Spacex key={(index)}  mission_name={value.mission_name} launch_year={value.launch_year} mission_patch_small={value.links.mission_patch_small}/>)}
         </div>
     );
 };
 
-export default SpaceX
+export default Space
--- react-app/src/components/spaceX.js
+++ react-app/src/components/spaceX.js
@@ -1,10 +1,14 @@
-
+import React from 'react';
 
 let Spacex = (props) => {
     let{mission_name, launch_year, mission_patch_small} = props;
     return (
-        <div>
-            {mission_name}  {launch_year}  {mission_patch_small}
+        <div className='main'>
+            <div>
+                <h1>{mission_name}  </h1>
+            <p>{launch_year}  </p>
+            </div>
+            <img src = {mission_patch_small} alt='img'/> 
         </div>
     );
     };
