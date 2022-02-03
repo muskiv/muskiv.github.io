46fc928ccf3d7a0ad9a2531562ce1d27c17b5896
muskiv <muskiv21@gmail.com>
2022-02-03 22:26:55 +0200

    2 file

---
+++ react-app/lesson6.js
@@ -0,0 +1,20 @@
+// import {IStarLink} from './StarLink.interface'
+// import {IUser} from './StarLink.interface'
+var user = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
+};
+function sum(a, b) {
+    return a + b;
+}
+function showSum(a, b) {
+    console.log(a + b);
+}
+function incAge(someUser, inc) {
+    someUser.age += inc;
+    return someUser;
+}
+console.log(sum(1, 2));
+showSum(2, 3);
+incAge(user, 2);
