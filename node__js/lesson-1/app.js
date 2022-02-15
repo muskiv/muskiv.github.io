f1fb0701c844ec8edf2544e00cfa53e06f74776e
muskiv <muskiv21@gmail.com>
2022-02-15 23:48:25 +0200

    nodejs-test

---
+++ node__js/lesson-1/app.js
@@ -0,0 +1,23 @@
+// const { greeting } = require('./helper.js');
+// greeting('Volodymyr');
+
+
+// console.log(__filename)
+// console.log(__dirname)
+// console.log(process.cwd(), 'process')
+
+// global.name = 'Vova';
+
+// console.log(name);
+// console.log(global.name);
+
+
+// require('./helper')
+// console.log(name);
+
+// const os = require('os');
+// console.log(os.cpus())
+// console.log(os.cpus().length)
+
+// const os = require('os');
+// console.log(os.arch())
---
+++ node__js/lesson-1/helper.js
@@ -0,0 +1,12 @@
+// function greeting(name) {
+//   console.log(`hello my name's ${name}`)
+// }
+
+// module.exports = { greeting };
+
+
+
+global.name = 'Vova';
+
+console.log(name);
+console.log(global.name);
