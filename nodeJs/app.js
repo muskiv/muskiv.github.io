26a5e37782ce39276aeb6ab4ab35464f283691c6
muskiv <muskiv21@gmail.com>
2022-02-18 06:16:19 +0200

    task 2 done, task 3 -50%

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -7,22 +7,108 @@ const path = require('path');
 const pathSomeTxt = path.join(__dirname, 'some.txt');
 const pathSomeTxt2 = path.join(__dirname, 'some2.txt');
 
-fs.writeFile(pathSomeTxt, 'Some Data, SACBTMKJGF;DCS SFBD', (err) => {
-  if (err) {
+// fs.writeFile(pathSomeTxt, 'Some Data, SACBTMKJGF;DCS SFBD', (err) => {
+//   if (err) {
+//     console.log(err);
+//     throw err;
+//   }
+
+//   fs.readFile(pathSomeTxt, 'utf8', (err, data)=>{
+//     if (err) {
+//     console.log(err);
+//     throw err;
+//     }
+
+//     fs.writeFile(pathSomeTxt2, data, (err) => {
+//       if (err) {
+//         console.log(err);
+//         throw err;
+//       }});
+//   });
+// });
+
+
+// 2. Створіть файл ( можете вручну ) заповніть його якимись даними
+// Прочитайте його, скопіюйте всі дані з нього і перенесіть їх в нову папку та файл в ній, старий файл видаліть після того як все завершиться. Також вийде callback hell
+
+
+const pathSomeTxt3 = path.join(__dirname, 'some3.txt');
+const pathSomeFile = path.join(__dirname, 'file2');
+const pathSomeTxt5 = path.join(__dirname, 'file2', 'some4.txt');
+
+
+// fs.writeFile(pathSomeTxt3, 'Some data sfvdg hdjlskmldkfg fdvfvdfvpf', (err) => {
+//   if (err) {
+//     console.log(err);
+//     throw err;
+//   }
+  
+//   fs.readFile(pathSomeTxt3, 'utf8', (err, data) =>{
+//     if (err) {
+//       console.log(err);
+//       throw err;
+//     }
+    
+//     fs.mkdir(pathSomeFile, {recursive: true}, (err) =>{
+//       if (err) {
+//         console.log(err);
+//         throw err;
+//       }
+
+//       fs.writeFile(pathSomeTxt5, data, (err) => {
+//         if (err) {
+//           console.log(err);
+//           throw err;
+//         }
+
+//         fs.unlink(pathSomeTxt3, (err) => {
+//           if(err) {
+//             console.log(err);
+//             throw err;
+//           }
+//         });
+//       });
+//     });
+//   });
+// });
+
+// 3. Створіть папку (можете вручну) напишіть скріпт який створить в ній якись дані (можуть бути нові папки і файли(в файли запишіть якусь дату) ) 
+// і напишіть функцію яка буде зчитувати папку і перевіряти якщо дані які в ній лежать - це файли тоді вам потрібно їх очистити, але не видаляти, якщо дані - це папки, вам потрібно їх перейменувати і додати до назви префікс _new
+
+const pathMainDir = path.join(__dirname, 'mainDir');
+const pathSomeDir2 = path.join(__dirname, 'mainDir', 'someDir2');
+const pathSomeDir3 = path.join(__dirname, 'mainDir', 'someDir3');
+const pathSomeTxt6 = path.join(__dirname, 'mainDir', 'someTxt6.txt');
+const pathSomeTxt7 = path.join(__dirname, 'mainDir', 'someTxt7.txt');
+
+
+fs.mkdir(pathMainDir, {recursive: true}, (err) => {
+  if (err){
     console.log(err);
     throw err;
   }
 
-  fs.readFile(pathSomeTxt, 'utf8', (err, data)=>{
-    if (err) {
-    console.log(err);
-    throw err;
-    }
+  fs.mkdir(pathSomeDir2, {recursive: true}, (err) => {
+    if (err){
+      console.log(err);
+      throw err;
+    }});
+  
+  fs.mkdir(pathSomeDir3, {recursive: true}, (err) => {
+    if (err){
+      console.log(err);
+      throw err;
+    }});
 
-    fs.writeFile(pathSomeTxt2, data, (err) => {
-      if (err) {
-        console.log(err);
-        throw err;
-      }});
-  });
+  fs.writeFile(pathSomeTxt6, 'TEST TEXT LOREM', (err) => {
+    if (err){
+      console.log(err);
+      throw err;
+    }});
+
+  fs.writeFile(pathSomeTxt7, 'kljmn hbgvfcdxs cdfvgbh njmki', (err) => {
+    if (err){
+      console.log(err);
+      throw err;
+    }});
 });
